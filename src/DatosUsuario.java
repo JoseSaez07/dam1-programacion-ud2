ec71bd86c2276a89b46478cc1e13dae842a24454
JoseSaez07 <146@alumnos.fpsumma.es>
2025-10-01 14:02:41 +0200

    s

--- src/DatosUsuario.java
+++ src/DatosUsuario.java
@@ -1,7 +1,28 @@
 import java.util.Scanner;
+import utilidades.ProcesadorTexto;
+import utilidades.Usuario;
 
 public class DatosUsuario {
-  Datos usuario = new Datos ();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-  usuario.close();
+        System.out.print("Introduce el Nombre: ");
+        String nombre = sc.nextLine();
+
+        System.out.print("Introduce el Apellido: ");
+        int apellido = sc.nextInt();    sc.nextLine();
+
+        System.out.print("Introduce tu correo electronico: ");
+        String email = sc.nextLine();
+
+        boolean emailValido = ProcesadorTexto.esEmailValido(email); 
+        if(emailValido){
+            Usuario usuario = new Usuario(nombre, apellido, email); 
+            usuario.mostrarInformacion();
+        }else{
+            System.out.println("No se puede crear por que el usuario no es valido");
+        }
+        sc.close();
+    }
 }
+
