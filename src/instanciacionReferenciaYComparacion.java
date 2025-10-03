32c009fb184b4181b3e62ee3df39010e7231e597
JoseSaez07 <146@alumnos.fpsumma.es>
2025-10-03 14:09:19 +0200

    s

---
+++ src/instanciacionReferenciaYComparacion.java
@@ -0,0 +1,26 @@
+import utilidades.Cancion;
+
+public class instanciacionReferenciaYComparacion {
+public static void main(String[] args) {
+// ================================
+// EJERCICIO 1: Crear objetos con new
+// ================================
+Cancion c1 = new Cancion();
+
+public Cancion() {
+private String titulo;
+private String artista;
+this.titulo ="Con título";
+this.titulo ="Sin título"; 
+    }
+
+}
+}
+
+
+
+// ================================
+// EJERCICIO 2: Comparación con ==
+// ================================
+
+cancion c3 =
