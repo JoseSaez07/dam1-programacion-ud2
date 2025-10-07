f3bc2125ec26885e504090f18da0099852620dd0
JoseSaez07 <146@alumnos.fpsumma.es>
2025-10-07 13:23:35 +0200

    Create TrabajandoConMetodos.java

---
+++ src/utilidades/TrabajandoConMetodos.java
@@ -0,0 +1,28 @@
+package utilidades;
+public class TrabajandoConMetodos {
+public class TrabajandoConMetodos {
+    public static void main(String[] args) {
+        DemoMetodos method = new DemoMetodos();
+        
+        // ================================
+        // EJERCICIO 1: Métodos void
+        // ================================
+        // 1. Llama al método saludar() de la clase DemoMetodos.
+        // Tu código aquí ↓     
+        method.saludar();   
+         // ================================
+        // EJERCICIO 2: Métodos con retorno
+        // ================================
+        // 1. Llama al método sumar(5, 3).
+        // 2. Guarda el resultado en una variable e imprímelo.
+        // 3. Llama al método presentar("Lucía") e imprime el resultado.
+        // Tu código aquí ↓
+        method.sumar(5,3);
+        System.out.print( method.sumar);
+         System.out.print( "Lucia");
+        
+   
+        }
+
+  }
+    } 
